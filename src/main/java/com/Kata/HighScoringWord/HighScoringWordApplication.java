6d30f83dcece19dea12fd047f8402ae9663c1cb7
Phil Thomas <philip.thomas@thoughtworks.com>
2019-01-23 15:02:24 -0500

    problem solved

--- src/main/java/com/Kata/HighScoringWord/HighScoringWordApplication.java
+++ src/main/java/com/Kata/HighScoringWord/HighScoringWordApplication.java
@@ -1,5 +1,8 @@
 package com.Kata.HighScoringWord;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,5 +13,7 @@ public class HighScoringWordApplication {
 		SpringApplication.run(HighScoringWordApplication.class, args);
 	}
 
+	public static String high(String s) {
+		return Arrays.stream(s.split(" ")).max(Comparator.comparingInt(a -> a.chars().map(i -> i - 96).sum())).get();
+	}
 }
-
--- src/test/java/com/Kata/HighScoringWord/HighScoringWordApplicationTests.java
+++ src/test/java/com/Kata/HighScoringWord/HighScoringWordApplicationTests.java
@@ -1,5 +1,7 @@
 package com.Kata.HighScoringWord;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,8 +12,51 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class HighScoringWordApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	public void high_scoring_word_expecting_zuknkizfedioxtqdi_test() {
+		String s = "ndauhyprfsihsgerml zuknkizfedioxtqdi";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("zuknkizfedioxtqdi", results);
+	}
+
+	@Test
+	public void high_scoring_word_expecting_piuoqaccjjnorcndxhd_test() {
+		String s = "qkmuzmdajzzqx piuoqaccjjnorcndxhd";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("piuoqaccjjnorcndxhd", results);
+	}
+
+	@Test
+	public void high_scoring_word_expecting_nothing_test() {
+		String s = "";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("", results);
+	}
+
+	@Test
+	public void high_scoring_word_expecting_taxi_test() {
+		String s = "man i need a taxi up to ubud";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("taxi", results);
+	}
+
+	@Test
+	public void high_scoring_word_expecting_volcano_test() {
+		String s = "what time are we climbing up to the volcano";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("volcano", results);
+	}
+
+	@Test
+	public void high_scoring_word_expecting_semynak_test() {
+		String s = "take me to semynak";
+		String results = HighScoringWordApplication.high(s);
+
+		assertEquals("semynak", results);
 	}
 
 }
-
