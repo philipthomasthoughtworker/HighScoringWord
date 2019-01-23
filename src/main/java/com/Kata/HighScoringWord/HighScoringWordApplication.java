1f18ea43260b7e500c703f9c92c7bcba0a0e80ba
Phil Thomas <philip.thomas@thoughtworks.com>
2019-01-23 13:27:16 -0500

    initial check-in

---
+++ src/main/java/com/Kata/HighScoringWord/HighScoringWordApplication.java
@@ -0,0 +1,14 @@
+package com.Kata.HighScoringWord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HighScoringWordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HighScoringWordApplication.class, args);
+	}
+
+}
+
